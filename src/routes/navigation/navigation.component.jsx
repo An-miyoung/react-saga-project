2d883454af962c20395449ab65984a7c7112c791
miyoung <noonchicat@gmail.com>
2024-06-27 14:55:28 +0900

    sign in with google

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import React, { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../aesets/crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link to="/" className="logo-container">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link to="/shop" className="nav-link">
+            SHOP
+          </Link>
+          <Link to="/sign-inn" className="nav-link">
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;

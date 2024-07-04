d90bdbae01137d8f63022b31ab0834d4d148d7af
miyoung <noonchicat@gmail.com>
2024-07-04 17:03:48 +0900

    firebase config 분리

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,121 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from "firebase/firestore";
+
+import { firebaseConfig } from "./firebase.config";
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Make a instance of Google provider
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(firebaseApp);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+// check user with email and password
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+// Initialize Firestore
+export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd,
+  field
+) => {
+  // collection 을 만들어 준다.
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    // collection 내부에 document를 만든다,
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    // 그 자리에 data를 내려놓는다.
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log("done");
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, "categories");
+  const q = query(collectionRef);
+
+  const querySanpshot = await getDocs(q);
+  const categoryMap = querySanpshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+  // docSnapshot.data() 는 { hats: { title: "hats", items: [{},{}...],
+  //                        jackets: {title:"jackets", items:[{},{}]}}} 형태
+
+  // 결과는 {hat: [{},{}....], jacket: [{},{}....]} 와 같은 형태로 변환
+  return categoryMap;
+};
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInfo = {}
+) => {
+  if (!userAuth) return;
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  // if user data does not exist
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInfo,
+      });
+    } catch (error) {
+      console.log("사용자등록 오류", error.message);
+    }
+  }
+  // if user data exist
+  return userDocRef;
+};
+
+// sign out
+export const signOutUser = async () => await signOut(auth);
+
+// auth listner
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);

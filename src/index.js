56e56e41afbac2d875c04f8dbf68907eb6c6b050
miyoung <noonchicat@gmail.com>
2024-07-11 15:07:30 +0900

    redux-persist

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persisto, store } from "./store/store";
 
 import "./index.scss";
 
@@ -12,9 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persisto}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -3,10 +3,11 @@ import {
   legacy_createStore as creteStore,
   applyMiddleware,
 } from "redux";
-// import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
 
-// currying function 은 변수와 또다른 함수를 전달하는 함수
+// currying function 은 변수와 또다른 함수를 전달하는 함수를 이용해 customizing logger구현
 const loggerMiddleware = (store) => (next) => (action) => {
   if (!action.type) {
     return next(action);
@@ -20,8 +21,18 @@ const loggerMiddleware = (store) => (next) => (action) => {
   console.log("next State : ", store.getState());
 };
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middlewares = [loggerMiddleware];
 
 const composedEnhancer = compose(applyMiddleware(...middlewares));
 
-export const store = creteStore(rootReducer, undefined, composedEnhancer);
+export const store = creteStore(persistedReducer, undefined, composedEnhancer);
+
+export const persisto = persistStore(store);
